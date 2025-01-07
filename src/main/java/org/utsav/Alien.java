524c370e4a1a1c1fd1f89b62f4344fd0995f3d76
utsav <utsav.zalavadiya07@gmail.com>
2025-01-07 08:05:41 +0530

    Alien Class commit with error of ApplicationContext

---
+++ src/main/java/org/utsav/Alien.java
@@ -0,0 +1,8 @@
+package org.utsav;
+
+public class Alien {
+
+    public void code(){
+        System.out.println("Coding");
+    }
+}
--- src/main/java/org/utsav/Main.java
+++ src/main/java/org/utsav/Main.java
@@ -1,7 +1,13 @@
 package org.utsav;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext();
+        Alien obj = context.getBean(Alien.class);
+        obj.code();
     }
 }
