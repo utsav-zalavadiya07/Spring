29393446085c47fe27a84416a976b133018ad0dd
Utsav_Tech <Utsav.zalavadiya54@gmail.com>
2025-01-07 17:01:21 +0530

    XML file added

--- src/main/java/org/utsav/Main.java
+++ src/main/java/org/utsav/Main.java
@@ -1,12 +1,11 @@
 package org.utsav;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext();
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
         Alien obj = context.getBean(Alien.class);
         obj.code();
     }
