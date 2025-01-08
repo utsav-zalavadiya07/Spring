6b1d8592dbcb77d535c74b22fe159ddf88205375
utsav <utsav.zalavadiya07@gmail.com>
2025-01-08 09:43:39 +0530

    Final XML based Configuartion commit

--- src/main/java/org/utsav/Alien.java
+++ src/main/java/org/utsav/Alien.java
@@ -1,7 +1,10 @@
 package org.utsav;
 
 public class Alien {
-
+    int age;
+    public  Alien(){
+        System.out.println("Constructor is calling");
+    }
     public void code(){
         System.out.println("Coding");
     }
--- src/main/java/org/utsav/Main.java
+++ src/main/java/org/utsav/Main.java
@@ -1,5 +1,4 @@
 package org.utsav;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,10 +6,17 @@ public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
-        Alien obj = context.getBean(Alien.class);
-        obj.code();
+        Alien obj1 = (Alien) context.getBean("alien1");
+        obj1.age=21;
+        System.out.println(obj1.age); //21
+        obj1.code();
 
-        Laptop laptop = context.getBean(Laptop.class);
+        Alien obj2 = (Alien) context.getBean("alien1");
+        System.out.println(obj2.age);  // 0
+
+        obj2.code();
+
+        Laptop laptop = (Laptop) context.getBean(Laptop.class);
         laptop.Compile();
     }
 }
